cfdc72f0fcc020bc927bccf5d58d9aab5dd78439
Luis <62965678+Tiquillo@users.noreply.github.com>
2020-10-15 19:42:11 -0600

    correción métodos carros.director

--- src/com/gmail/markorovi24/Carros/Director.java
+++ src/com/gmail/markorovi24/Carros/Director.java
@@ -3,62 +3,62 @@ package com.gmail.markorovi24.Carros;
 public class Director {
     public void createBarato(IBuilder builder){
         builder.reset();
-        builder.setMarca("Toyota");
+        builder.setMarca("Hyundai");
         builder.setPasajeros(4);
-        builder.setCombustible("Diesel");
+        builder.setCombustible("Regular");
         builder.setPrecio(4000);
-        builder.setPuertas(2);
+        builder.setPuertas(4);
         builder.setTipo("Automovil");
     }
 
-    public void createBuseta(IBuilder builder){
+    public void createMicroBus(IBuilder builder){
         builder.reset();
-        builder.setMarca("Toyasdfasdfota");
-        builder.setPasajeros(4);
-        builder.setCombustible("Dieseasdghrefgghjertyjl");
-        builder.setPrecio(4000);
-        builder.setPuertas(2);
-        builder.setTipo("Automovil");
+        builder.setMarca("Suzuki");
+        builder.setPasajeros(12);
+        builder.setCombustible("Super");
+        builder.setPrecio(6000);
+        builder.setPuertas(3);
+        builder.setTipo("MicroBus");
     }
 
     public void createCamion(IBuilder builder){
         builder.reset();
-        builder.setMarca("Toyasdfasdfota");
+        builder.setMarca("Jac");
         builder.setPasajeros(4);
-        builder.setCombustible("Dieseasdghrefgghjertyjl");
-        builder.setPrecio(4000);
+        builder.setCombustible("Diesel");
+        builder.setPrecio(7000);
         builder.setPuertas(2);
-        builder.setTipo("Automovil");
+        builder.setTipo("Camión");
     }
 
     public void createMoto(IBuilder builder){
         builder.reset();
-        builder.setMarca("Toyasdfasdfota");
-        builder.setPasajeros(4);
-        builder.setCombustible("Dieseasdghrefgghjertyjl");
-        builder.setPrecio(4000);
-        builder.setPuertas(2);
-        builder.setTipo("Automovil");
+        builder.setMarca("Yamaha");
+        builder.setPasajeros(2);
+        builder.setCombustible("Regular");
+        builder.setPrecio(2000);
+        builder.setPuertas(0);
+        builder.setTipo("Motocicleta");
     }
 
     public void createCuadra(IBuilder builder){
         builder.reset();
-        builder.setMarca("Toyasdfasdfota");
-        builder.setPasajeros(4);
-        builder.setCombustible("Dieseasdghrefgghjertyjl");
-        builder.setPrecio(4000);
-        builder.setPuertas(2);
-        builder.setTipo("Automovil");
+        builder.setMarca("Honda");
+        builder.setPasajeros(2);
+        builder.setCombustible("Mezcla");
+        builder.setPrecio(3000);
+        builder.setPuertas(0);
+        builder.setTipo("Cuadraciclo");
     }
 
     public void createPickUp(IBuilder builder){
         builder.reset();
-        builder.setMarca("Toyasdfasdfota");
+        builder.setMarca("Mitsubishi");
         builder.setPasajeros(4);
-        builder.setCombustible("Dieseasdghrefgghjertyjl");
-        builder.setPrecio(4000);
+        builder.setCombustible("Diesel");
+        builder.setPrecio(6000);
         builder.setPuertas(2);
-        builder.setTipo("Automovil");
+        builder.setTipo("Pick Up");
     }
 
     public void createScooter(IBuilder builder){
