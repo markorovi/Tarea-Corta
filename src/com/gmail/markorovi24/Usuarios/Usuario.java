62fd5be1196e2271521dbc0914a5c4e763eaf5e7
Luis <62965678+Tiquillo@users.noreply.github.com>
2020-10-15 18:04:13 -0600

    dateformat

--- src/com/gmail/markorovi24/Usuarios/Usuario.java
+++ src/com/gmail/markorovi24/Usuarios/Usuario.java
@@ -1,10 +1,12 @@
 package com.gmail.markorovi24.Usuarios;
 
+import java.text.DateFormat;
+
 public class Usuario {
     protected String perfilFacebook;
     protected int cedula;
     protected int numeroTelefono;
-    protected String fechaVencimiento;
+    protected DateFormat fechaVencimiento;
     protected String direccionResidencia;
     protected String linkFotos;
     protected boolean deBaja;
